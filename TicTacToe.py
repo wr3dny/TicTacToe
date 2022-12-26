af34c63f5ad21ef7a66c43974e99ef73ea212089
Pawe Pawtel <pawel.pawtel@gmail.com>
2022-12-26 12:45:31 +0100

    Trying to finish it

--- TicTacToe.py
+++ TicTacToe.py
@@ -13,21 +13,78 @@ print(board[1][1])
 
 def board_display(name1, name2):
     cl = max(len(name1), len(name2))
-    print(f'{name1 : ^{cl}}')
+    print(f'{name1 : ^{cl}} -  playing X')
     vs = 'vs'
     print(vs.center(cl))
-    print(f'{name2 : ^{cl}}')
+    print(f'{name2 : ^{cl}} -  playing O')
     print(' ')
-    for l in board:
-        print(*l)
+    for b in board:
+        print(*b)
 
 
-def winning_con():
+def winning_con_x():
+
+    win_x_row_a = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', 'X', '|', 'X', '|', 'X', '|'],
+             ['B', '|', '.', '|', '.', '|', '.', '|'], ['C', '|', '.', '|', '.', '|', '.', '|']]
+
+    win_x_row_b = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', '.', '|', '.', '|'],
+             ['B', '|', 'X', '|', 'X', '|', 'X', '|'], ['C', '|', '.', '|', '.', '|', '.', '|']]
+
+    win_x_row_c = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', '.', '|', '.', '|'],
+             ['B', '|', '.', '|', '.', '|', '.', '|'], ['C', '|', 'X', '|', 'X', '|', 'X', '|']]
+
+    win_x_column_1 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', 'X', '|', '.', '|', '.', '|'],
+             ['B', '|', 'X', '|', '.', '|', '.', '|'], ['C', '|', 'X', '|', '.', '|', '.', '|']]
+
+    win_x_column_2 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', 'X', '|', '.', '|'],
+             ['B', '|', '.', '|', 'X', '|', '.', '|'], ['C', '|', '.', '|', 'X', '|', '.', '|']]
+
+    win_x_column_3 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', '.', '|', 'X', '|'],
+             ['B', '|', '.', '|', '.', '|', 'X', '|'], ['C', '|', '.', '|', '.', '|', 'X', '|']]
+
+    win_x_diagonal_1 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', 'X', '|', '.', '|', '.', '|'],
+             ['B', '|', '.', '|', 'X', '|', '.', '|'], ['C', '|', '.', '|', '.', '|', 'X', '|']]
+
+    win_x_diagonal_2 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', '.', '|', 'X', '|'],
+             ['B', '|', '.', '|', 'X', '|', '.', '|'], ['C', '|', 'X', '|', '.', '|', '.', '|']]
+
+    win_x = win_x_row_a + win_x_row_b + win_x_row_c + win_x_column_1 + win_x_column_2 + win_x_column_3 +\
+            win_x_diagonal_1 + win_x_diagonal_2
+
+    return win_x
+
+def winning_con_o():
+
+    win_o_row_a = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', 'O', '|', 'O', '|', 'O', '|'],
+                   ['B', '|', '.', '|', '.', '|', '.', '|'], ['C', '|', '.', '|', '.', '|', '.', '|']]
+
+    win_o_row_b = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', '.', '|', '.', '|'],
+                   ['B', '|', 'O', '|', 'O', '|', 'O', '|'], ['C', '|', '.', '|', '.', '|', '.', '|']]
+
+    win_o_row_c = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', '.', '|', '.', '|'],
+                   ['B', '|', '.', '|', '.', '|', '.', '|'], ['C', '|', 'O', '|', 'O', '|', 'O', '|']]
+
+    win_o_column_1 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', 'O', '|', '.', '|', '.', '|'],
+                      ['B', '|', 'O', '|', '.', '|', '.', '|'], ['C', '|', 'O', '|', '.', '|', '.', '|']]
+
+    win_o_column_2 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', 'O', '|', '.', '|'],
+                      ['B', '|', '.', '|', 'O', '|', '.', '|'], ['C', '|', '.', '|', 'O', '|', '.', '|']]
+
+    win_o_column_3 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', '.', '|', 'O', '|'],
+                      ['B', '|', '.', '|', '.', '|', 'O', '|'], ['C', '|', '.', '|', '.', '|', 'O', '|']]
+
+    win_o_diagonal_1 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', 'O', '|', '.', '|', '.', '|'],
+                        ['B', '|', '.', '|', 'O', '|', '.', '|'], ['C', '|', '.', '|', '.', '|', 'O', '|']]
+
+    win_o_diagonal_2 = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', '.', '|', '.', '|', 'O', '|'],
+                        ['B', '|', '.', '|', 'O', '|', '.', '|'], ['C', '|', 'O', '|', '.', '|', '.', '|']]
+
+    win_o = win_o_row_a + win_o_row_b + win_o_row_c + win_o_column_1 + win_o_column_2 + win_o_column_3 + \
+            win_o_diagonal_1 + win_o_diagonal_2
+
+    return win_o
 
 
-    win_x = [[' ', '|', '1', '|', '2', '|', '3', '|'], ['A', '|', 'X', '|', '.', '|', '.', '|'],
-         ['B', '|', 'X', '|', '.', '|', '.', '|'], ['C', '|', 'X', '|', '.', '|', '.', '|']]
-    pass
 
 
 def play(n1, n2):
@@ -36,18 +93,19 @@ def play(n1, n2):
     row = input('Row: ')
     row = row.capitalize()
     column = int(input('Column: '))
+
     if row == 'A':
         row = 1
     elif row == 'B':
         row = 2
     elif row == 'C':
         row = 3
-    while (row, column) in used_spots:
-        row = input('Row: ')
-        column = int(input('Column: '))
 
     used_spots.append((row, column))
     board[row][column+column] = 'X'
+
+
+
 
     print(f'{n2}, place "O" on the board.')
     row = input('Row: ')
@@ -59,13 +117,45 @@ def play(n1, n2):
         row = 2
     elif row == 'C':
         row = 3
-    while (row, column) in used_spots:
-        print('Not possible. Already in use')
-        row = input('Row: ')
-        column = int(input('Column: '))
 
     used_spots.append((row, column))
     board[row][column+column] = 'O'
+
+    print(used_spots)
+    # while (row, column) in used_spots:
+    #     print('Not possible. Already in use')
+    #     row = input('Row: ')
+    #     column = int(input('Column: '))
+    #
+
+    #
+    #
+    #
+    #
+    #
+    # while (row, column) in used_spots:
+    #     row = input('Row: ')
+    #     column = int(input('Column: '))
+    #
+
+    #
+    # print(f'{n2}, place "O" on the board.')
+    # row = input('Row: ')
+    # row = row.capitalize()
+    # column = int(input('Column: '))
+    # if row == 'A':
+    #     row = 1
+    # elif row == 'B':
+    #     row = 2
+    # elif row == 'C':
+    #     row = 3
+    # while (row, column) in used_spots:
+    #     print('Not possible. Already in use')
+    #     row = input('Row: ')
+    #     column = int(input('Column: '))
+    #
+    # used_spots.append((row, column))
+    # board[row][column+column] = 'O'
 
     # print(f'{n1}, place "X" on the board.')
     # row = input('Row: ')
@@ -141,7 +231,11 @@ def main():
     name_1 = input('Player 1 name: ')
     name_2 = input('Player 2 name: ')
 
+    player_1_win = winning_con_x()
+    player_2_win = winning_con_o()
+
     board_display(name_1, name_2)
+
     play(name_1, name_2)
     board_display(name_1, name_2)
     play(name_1, name_2)
